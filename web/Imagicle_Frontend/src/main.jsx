f0474325c82f7570f451b6a2206d0e0b386a7ff0
Kenneth Le <kennethle@Kenneths-MacBook-Pro-2.local>
2025-09-27 00:07:08 -0400

    Adjusted files

--- web/Imagicle_Frontend/src/main.jsx
+++ web/Imagicle_Frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LandingPage from './pages/LandingPage.jsx'
+import App from './App.jsx' // this will be your viewer page for /app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<App />} />
+        {/* if your old link points to /wkp, redirect it */}
+        <Route path="/wkp" element={<Navigate to="/app" replace />} />
+        {/* 404 */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ web/Imagicle_Frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import {
+    Play,
+    ChevronDown,
+    CheckCircle,
+    Linkedin,
+    Camera,
+    Target,
+    TrendingUp,
+    Shield,
+} from "lucide-react";
